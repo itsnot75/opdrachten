607099f3c6b3cd4a987561d965e4d4716bf59d13
Levinio Sumter <2154356@talnet.nl>
2023-05-12 16:25:16 +0200

    first commit

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php 
+
+$leeftijd = 12;
+$lengte = 10;
+
+$resultaat = $leeftijd + $lengte;
+
+echo "het resultaat is" . $resultaat;
+?>
+
+<?php 
+
+$leeftijd = 12;
+$lengte = 10;
+
+$resultaat = $leeftijd * $lengte;
+
+echo "het resultaat is" . $resultaat;
+?>
+
