b55d33755bfffb1c62f66c0af66311c933526441
Levinio Sumter <2154356@talnet.nl>
2023-06-02 15:05:25 +0200

    pp

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+// for ($i = 1; $i <= 50; $i++) {
+//     // echo $i . " ";} 
+
+
+
+
+
+
+    // $namen = array( "lebron" , "kobe" , "jeronimo" , "rick" , "curry" , "dwayne wade" , "rico" , "tim" , "mik");
+    // foreach ($namen as $namen) {
+
+
+        $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']; 
+      
+        for ($i = 0; $i < count($maanden); $i++)  {
+         echo "De maand is " . ($i + 1). " " . $maanden[ $i
+         
+         ] . "<br>";
+        
+
+        }
+
+
+     
+
+
+?>
+
+    
+
+
+  
